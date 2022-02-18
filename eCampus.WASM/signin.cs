54a84ce5c108571b69aacd47bbcc146096b852f5
WeCodemBusiness <emmanuelbwalyaemb@gmail.com>
2022-02-18 10:25:10 +0200

    SignIn logic has been commited

---
+++ eCampus.WASM/signin.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace eCampus.WASM
+{
+    public class signin
+    {
+
+        public string Username { get; set;}
+        public string Email { get; set;}
+        public string Password { get; set;}
+
+
+    }
+}
